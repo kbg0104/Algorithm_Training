e19c8715cbf8573102ea95ee8ba3d762115ec684
kbg0104 <kbg0104@naver.com>
2018-09-22 17:53:35 +0900

    갱신(2018-09-22)

---
+++ 알고리이즘/알고리이즘/소수의 합.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+	int* arr;
+	int i, j;
+	arr = (int*)calloc(sizeof(int), n + 1);
+	for (i = 1; i <= n; i++) {
+		arr[i] = i;
+	}
+	/**********************************/
+	for (i = 2; i <= sqrt(n); i++) {
+		if (arr[i] == 0)
+			continue;
+		else
+			for (j = i; j <= n; j += arr[i])
+				if (j == arr[i])
+					continue;
+				else
+					arr[j] = 0;
+	}
+	/**********************************/
+	for (i = 2; i <= n; i++)
+		if (arr[i] != 0)
+			answer += arr[i];
+
+	cout << answer;
+	return answer;
+}
+void main()
+{
+	int n = 7;
+	solution(n);
+
+
+
+}
---
+++ 알고리이즘/알고리이즘/이상한 문자 만들기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+string solution(string s) {
+	string answer = "";
+	vector<string> list;
+	istringstream sum(s);
+	int i;
+	for (i = 0; i <= sum.size(); i++) {
+
+	}
+	for (i = 0; i <= list.size(); i++) {
+		cout << list.at(i);
+	}
+	return answer;
+}
+void main() {
+	int n;
+	string s = "";
+	cin >> s;
+	solution(s);
+
+}
